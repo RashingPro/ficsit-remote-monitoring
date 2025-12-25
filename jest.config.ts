024ac744310cb6bbeb74db0cfc4e1ad6395e372b
RashingPro <rashingpro@yandex.ru>
2025-12-25 18:45:07 +0300

    build: implement blank Jest

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import { createDefaultPreset } from "ts-jest";
+import { JestConfigWithTsJest, pathsToModuleNameMapper } from "ts-jest";
+import { compilerOptions } from "./tsconfig.json";
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+export default {
+    testEnvironment: "node",
+    transform: {
+        ...tsJestTransformCfg
+    },
+    rootDir: "./test",
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/../" })
+} as JestConfigWithTsJest;
